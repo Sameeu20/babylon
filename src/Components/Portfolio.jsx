abb039729115d09a13c21e9cf0e548c56ddc7f57
Sameeu20 <abdulsameeu9@gmail.com>
2024-01-28 02:53:18 +0530

    Minor adjustment to imagegrid

--- src/Components/Portfolio.jsx
+++ src/Components/Portfolio.jsx
@@ -12,8 +12,8 @@ const Portfolio = () => {
       <Header/>
       <img className = 'w-200 ' src={bg}/>
       </div>
-      <div className='grid grid-cols-3 mt-32 ml-14 gap-6'>
-        <div className='col-span-1.85'>
+      <div className='grid grid-cols-3 mt-32 ml-14 gap-2  '>
+        <div className='col-span-1'>
           <h3 className='text-black font-montserrat text-6xl font-semibold leading-[130%] tracking-tighter uppercase'>IMAGES</h3>
           <p className='text-black text-justify font-montserrat text-base font-normal leading-[185%] w-96'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam eu turpis molestie, dictum est a, mattis tellus. Sed dignissim, metus nec fringilla accumsan, risus sem sollicitudin lacus, ut interdum tellus elit sed risus. Maecenas eget condimentum velit, sit amet feugiat lectus. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Praesent auctor purus luctus enim egestas, ac scelerisque ante pulvinar. Donec ut rhoncus ex. Suspendisse ac rhoncus nisl, eu tempor urna. Curabitur vel bibendum lorem. Morbi convallis convallis diam sit amet lacinia. Aliquam in elementum tellus.
           Curabitur temulum venenatis. Suspendisse quis arcu sem. Aenean feugiat ex eu vestibulum o.            
