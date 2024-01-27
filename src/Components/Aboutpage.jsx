22fa21e68cf337fb6e711b46662b2a9175a75a7a
Sameeu20 <abdulsameeu9@gmail.com>
2024-01-27 20:01:04 +0530

    Added Why choose us cards section with relevant styles

--- src/Components/Aboutpage.jsx
+++ src/Components/Aboutpage.jsx
@@ -1,41 +1,56 @@
-import React from 'react'
-import bg from '../assets/about.png'
+import React from 'react';
+import bg from '../assets/about.png';
 import Header from './Header';
-import about from '../assets/aboutbg.png'
+import about from '../assets/aboutbg.png';
+import Whychoose from './Whychoose';
 
 const Aboutpage = () => {
+  const cardsData = [
+    { heading: 'Free Delivery', description: 'Lorem ipsum dolor sit amet.' },
+    { heading: 'Free Delivery', description: 'Lorem ipsum dolor sit amet.' },
+    { heading: 'Support 24/7', description: 'Lorem ipsum dolor sit amet.' },
+    { heading: 'Support 24/7', description: 'Lorem ipsum dolor sit amet.' },
+    { heading: 'Free Delivery', description: 'Lorem ipsum dolor sit amet.' },
+    { heading: 'Free Delivery', description: 'Lorem ipsum dolor sit amet.' },
+  ];
+
   return (
-    <div className='h-[100vh] w-full mx-auto'>
-     <div >
-      <Header/>
-      <img className = 'w-200 ' src={bg}/>
+    <div className='h-screen w-full mx-auto'>
+      <div>
+        <Header />
+        <img className='w-200' src={bg} alt='Background' />
       </div>
-      <div className="h-[100vh] w-full mx-auto grid grid-cols-2">
-     <div className=" p-5 m-20 space-y-4 lg:pt-14">
-  
-        <h1 className=" px-8 font-bold text-4xl text-center md:text-start">
-          About
+      <div className='h-screen w-full mx-auto grid grid-cols-2 gap-8'>
+        <div className='p-5 m-20 space-y-4 lg:pt-14'>
+          <h1 className='px-8 font-bold text-4xl text-center md:text-start'>
+            About
+          </h1>
+          <p className='px-8'>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi
+            architecto quisquam delectus minima perferendis nulla quia nisi
+            laborum, exercitationem cum quo accusantium, impedit sed. Dicta, quo
+            molestias omnis reprehenderit quae animi? Explicabo quasi accusamus
+            laboriosam temporibus delectus, aut a? Quasi?
+          </p>
+        </div>
+        <div className='py-5'>
+          <img className='mt-[30px]' src={about} alt='About' />
+        </div>
+      </div>
+      <div className='w-full flex flex-col items-center justify-center'>
+        <h1 className='text-black text-shadow-md font-montserrat text-2xl md:text-3xl lg:text-4xl xl:text'>
+          Why Choosing Us
         </h1>
-        <p className="px-8">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi
-          architecto quisquam delectus minima perferendis nulla quia nisi
-          laborum, exercitationem cum quo accusantium, impedit sed. Dicta, quo
-          molestias omnis reprehenderit quae animi? Explicabo quasi accusamus
-          laboriosam temporibus delectus, aut a? Quasi?
-        </p>
-    </div>
-    <div className="py-5">
-      <img className='mt-[30px]' src={about}/>
-    </div>
-
-   </div>
-  < div className='h-[80vh] w-full flex justify-center'>
-      <h1 className='font-bold text-3xl'>
-        Why choosing US
-      </h1>
-    </div>
+        <div className='flex flex-wrap gap-x-317 gap-y-8 justify-center ml-72'>
+          {cardsData.map((card, index) => (
+            <div key={index} className='w-1/2'>
+              <Whychoose {...card} />
+            </div>
+          ))}
+        </div>
       </div>
-  )
-}
+    </div>
+  );
+};
 
-export default Aboutpage
+export default Aboutpage;
---
+++ src/Components/Whychoose.jsx
@@ -0,0 +1,13 @@
+// Card.jsx
+import React from 'react';
+
+const Whychoose = ({ heading, description }) => {
+  return (
+    <div className="bg-white p-6 m-4 rounded ">
+      <h2 class="text-gray-700 font-montserrat text-2xl font-semibold">{heading}</h2>
+      <p class="text-gray-700 font-montserrat text-base font-normal">{description}</p>
+    </div>
+  );
+};
+
+export default Whychoose;
