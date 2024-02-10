eb3b25ceda0da9995b59351cd17a0e5fd6b9f11f
FahasBasil <fahasbasil007@gmail.com>
2024-02-10 15:06:52 +0530

    responsive

--- src/Components/Filter.jsx
+++ src/Components/Filter.jsx
@@ -25,13 +25,15 @@ const Filter = ({ onSelectService }) => {
     <div className='flex flex-col items-center w-full mb-[100px]'>
       <div className='absolute bg-gray-200 h-[68px] w-[1240px] rounded-[100px] mt-[52px]' />
       <div className='absolute bg-gray-200 h-[68px] w-[1240px] rounded-[100px] mt-[125px]' />
-      <div className='absolute bg-gray-200 h-[60px] w-[240px] rounded-[100px] mt-[200px]' />
+      <div className='absolute bg-gray-200 h-[60px] max-sm:w-[500px] md:w-[1000px] w-[240px] rounded-[100px] mt-[200px]' />
+      <div className='lg:hidden absolute bg-gray-200 h-[60px] max-sm:w-[500px] md:w-[1000px] w-[240px] rounded-[100px] mt-[270px]' />
+      <div className=' lg:hidden absolute bg-gray-200 h-[60px] max-sm:w-[500px] md:w-[1000px] w-[240px] rounded-[100px] mt-[345px]' />
       <div className='relative flex flex-wrap justify-center gap-4 py-3 mt-[50px]  rounded-[50px]  mx-[25px] '>
         
         {ServiceData.map((service) => (
           <button
             key={service.id}
-            className={`w-[230px] h-12 rounded-[30px] border-none px-2  mb-2 hover:bg-white ${activeButton === service.id ? 'bg-white' : 'bg-gray-200'}`}
+            className={`max-sm:w-[130px] w-[230px] h-12 rounded-[30px] border-none px-2  mb-2 hover:bg-white ${activeButton === service.id ? 'bg-white' : 'bg-gray-200'}`}
             onClick={() => handleButtonClick(service.id)}
           >
             {service.title}
